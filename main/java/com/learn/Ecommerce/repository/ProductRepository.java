a0e77c81d962b8f418041a096de1eb5f0abe5155
Shriya-Dubey02 <shriyadubey220@gmail.com>
2024-11-28 14:50:37 +0530

    28 Nov

--- main/java/com/learn/Ecommerce/repository/ProductRepository.java
+++ main/java/com/learn/Ecommerce/repository/ProductRepository.java
@@ -1,9 +1,23 @@
 package com.learn.Ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
+	List<Product> findByProductPriceGreaterThan(int price);
+	
+	List<Product> findByProductPriceLessThan(int price);
+	
+	List<Product> findByProductPriceLessThanEqual(int price);
+	
+	List<Product> findByProductPriceBetween(int startPrice,int endPrice);
+	
+	List<Product> findByProductNameStartingWith(String pname);
+	
+	
 
 }
