e5aeda9c0d4f220035383e413fc6476a91418362
Shriya-Dubey02 <shriyadubey220@gmail.com>
2024-12-06 09:17:09 +0530

    6 Dec

---
+++ main/java/com/learn/Ecommerce/productProjection/ProductProjection.java
@@ -0,0 +1,6 @@
+package com.learn.Ecommerce.productProjection;
+
+public interface ProductProjection {
+	String getProductName();
+
+}
--- main/java/com/learn/Ecommerce/repository/ProductRepository.java
+++ main/java/com/learn/Ecommerce/repository/ProductRepository.java
@@ -3,9 +3,13 @@ package com.learn.Ecommerce.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.learn.Ecommerce.entity.Product;
+import com.learn.Ecommerce.productProjection.ProductProjection;
 
+
+@RepositoryRestResource(excerptProjection = ProductProjection.class)
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 	
 	List<Product> findByProductPriceGreaterThan(int price);
